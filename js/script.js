321a822ccf9d57f389e6302481b0362731e34f65
giorgia-rondinelli <g.rondinelli4@gmail.com>
2024-02-29 17:34:21 +0100

    [add] difficoltà 3

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 // bottoni
 const btnStart= document.getElementById('start')
 const livello2=document.getElementById('difficolta2')
+const livello3=document.getElementById('difficolta3')
+
+
 
 // elementi 
 const container=document.getElementById('square-container')
@@ -9,6 +12,7 @@ const container=document.getElementById('square-container')
 
 btnStart.addEventListener('click', start)
 livello2.addEventListener('click', difficolta2)
+livello3.addEventListener('click', difficolta3)
  
 
 
@@ -73,3 +77,26 @@ function generateSquare2(num){
   }) 
   
   return cell}
+
+
+  function difficolta3(){
+    reset()
+     for(let i=1; i<=49; i++){
+   const square=generateSquare3(i)
+   container.append(square)
+   
+  }
+  }
+
+  function generateSquare3(num){
+    const cell= document.createElement('div')
+    cell.className ='square3';
+    cell.numero=num
+  
+    cell.addEventListener('click', function(){
+      console.log(num)
+      this.innerHTML=this.numero
+      this.classList.add('blue')
+    }) 
+    
+    return cell}
